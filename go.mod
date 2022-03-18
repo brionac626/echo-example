c62ed0b05cd724b7c97bb8a1566f761260925c60
Hsien-Kuei Chiu <hsien.chiu@a5labs.co>
2022-03-18 13:21:25 +0800

    add: middleware and handler using zerolog

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/brionac626/testEcho
 
 go 1.16
+
+require (
+	github.com/labstack/echo/v4 v4.7.2
+	github.com/rs/zerolog v1.26.1
+)
--- main.go
+++ main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	initLogger()
+	e := echo.New()
+	e.GET("/", echoHandler, addLoggerToContext)
+
+	log.Fatal().Err(e.Start(":8080")).Send()
+}
+
+func initLogger() {
+	log.Logger = zerolog.New(os.Stdout).With().Str("app", "test_echo").Logger()
+}
+
+func addLoggerToContext(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		l := log.Logger.With().Str("middleware", "add logger to context").Logger()
+		c.SetRequest(c.Request().WithContext(l.WithContext(c.Request().Context())))
+		return next(c)
+	}
+}
+
+func echoHandler(c echo.Context) error {
+	logger := zerolog.Ctx(c.Request().Context())
+	logger.Info().Str("endpoint", c.Path()).Msg("c in handler")
+	return nil
+}
